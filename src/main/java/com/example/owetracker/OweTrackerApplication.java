b03bdb31cdfa6857ca90375e37a97f686acc1ebb
MARTO <martynasled@gmail.com>
2024-09-09 18:58:02 +0300

    initial commit

---
+++ src/main/java/com/example/owetracker/OweTrackerApplication.java
@@ -0,0 +1,17 @@
+package com.example.owetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OweTrackerApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(OweTrackerApplication.class, args);
+
+
+
+    }
+
+}
---
+++ src/test/java/com/example/owetracker/OweTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.owetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OweTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
