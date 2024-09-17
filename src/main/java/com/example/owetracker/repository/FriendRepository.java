012ef3331096413d58daf471740caf2cf949d588
elzpil <elze.pilibaityte@gmail.com>
2024-09-17 15:13:07 +0300

    adds friend request, can see pending requests

--- src/main/java/com/example/owetracker/repository/FriendRepository.java
+++ src/main/java/com/example/owetracker/repository/FriendRepository.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface FriendRepository extends JpaRepository<Friend, Integer> {
@@ -19,11 +20,17 @@ public interface FriendRepository extends JpaRepository<Friend, Integer> {
 
     // Find all friends for a given user where the user is either the userId or the friendId
     List<Friend> findByUserIdOrFriendId(Integer userId, Integer friendId);
+    List<Friend> findByUserIdOrFriendIdAndStatus(Integer userId, Integer friendId, String status);
 
     @Query("SELECT SUM(e.amountOwed) FROM ExpenseUser e WHERE e.user.id = :userId")
     BigDecimal getTotalOwedByUser(@Param("userId") Integer userId);
 
     @Query("SELECT SUM(e.amount) FROM Expense e WHERE e.paidBy.id = :userId")
     BigDecimal getTotalOwedToUser(@Param("userId") Integer userId);
+
+
+    Optional<Friend> findByUserIdAndFriendId(Integer userId, Integer friendId);
+
+    List<Friend> findByFriendIdAndStatus(Integer friendId, String status);
 
 }
