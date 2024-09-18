31ca6e1326ee22a0af0516c3d55cf80661a3e907
MARTO <martynasled@gmail.com>
2024-09-18 12:42:36 +0300

    moved all the group logic to master

---
+++ src/main/java/com/example/owetracker/model/GroupMembership.java
@@ -0,0 +1,52 @@
+package com.example.owetracker.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "group_memberships")
+public class GroupMembership {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "group_id")
+    private Group group;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public GroupMembership() {
+    }
+
+    public GroupMembership(Group group, User user) {
+        this.group = group;
+        this.user = user;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
---
+++ src/main/java/com/example/owetracker/repository/GroupRepository.java
@@ -0,0 +1,23 @@
+package com.example.owetracker.repository;
+
+import com.example.owetracker.model.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface GroupRepository extends JpaRepository<Group, Integer> {
+
+    List<Group>findByTitle(String title);
+    List<Group> findByTitleContaining(String title);
+
+    @Query("SELECT g FROM Group g JOIN GroupMembership gm ON g = gm.group WHERE gm.user.id = :userId")
+    List<Group> findGroupsByUserId(@Param("userId") Integer userId);
+
+    void deleteById(Integer groupId);
+
+
+}
